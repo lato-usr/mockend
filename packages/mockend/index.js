6c2c3f9f6f9e432cb4f953dbc50ce489d5574cd0
lato-usr <64500314+lato-usr@users.noreply.github.com>
2020-05-03 09:55:01 +0200

    remove log

--- packages/mockend/index.js
+++ packages/mockend/index.js
@@ -15,7 +15,6 @@ fs.mkdirSync(tmpDir)
 app.use(express.static(tmpDir))
 
 app.post('/:name', (req, res, next) => {
-  console.log(req.params.name)
   fs.writeFile(path.resolve(tmpDir, req.params.name), '', (err) => {
     if (err) next(err)
     res.sendStatus(201)
